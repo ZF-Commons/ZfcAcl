ff103758a18c6019c7722d683ca7fffaf6a5c105
Evan Coury <me@evancoury.com>
2012-01-23 13:00:28 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+ZfcUserAcl
+==========
+Version 0.0.1 Created by Evan Coury and the ZF-Commons team
+
+**WARNING** This module is not functional yet.
+
+Introduction
+------------
+
+ZfcUserAcl is a module for Zend Framework 2 which provides a simple, highly
+flexible ACL implementation that works with
+[ZfcUser](https://github.com/ZF-Commons/ZfcUser) out of the box.
---
+++ src/ZfcUserAcl/Model/Role.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace ZfcUserAcl\Model;
+
+interface Role
+{
+
+    /**
+     * getRoleId 
+     * 
+     * @return int
+     */
+    public function getRoleId();
+
+    /**
+     * setRoleId 
+     * 
+     * @param int $roleId 
+     * @return Role
+     */
+    public function setRoleId($roleId);
+
+    /**
+     * getName 
+     * 
+     * @return string
+     */
+    public function getName();
+
+    /**
+     * setName 
+     * 
+     * @param string $name 
+     * @return Role
+     */
+    public function setName($name);
+
+    /**
+     * getPriority 
+     * 
+     * @return Role
+     */
+    public function getPriority();
+
+    /**
+     * setPriority 
+     * 
+     * @param int $priority 
+     * @return Role
+     */
+    public function setPriority($priority);
+}
