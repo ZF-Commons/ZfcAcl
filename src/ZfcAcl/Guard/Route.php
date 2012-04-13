ee850e4c9fa7b376defefb56cab04426d2b79bfa
Marco Pivetta <ocramius@gmail.com>
2012-04-13 18:37:37 +0200

    Implementing ZfcAclAwareInterface in base guards

--- src/ZfcAcl/Guard/Route.php
+++ src/ZfcAcl/Guard/Route.php
@@ -2,51 +2,66 @@
 
 namespace ZfcAcl\Guard;
 
-use Zend\Mvc\MvcEvent,
-    ZfcAcl\Exception\UnauthorizedException,
-    Exception as NoRouteResourceFoundException;
+use Zend\Mvc\MvcEvent;
+use ZfcAcl\Exception\UnauthorizedException;
+use Exception as NoRouteResourceFoundException;
+use ZfcAcl\Service\ZfcAclAwareInterface;
 
-class Route implements Guard {
-    protected $routeResourceMapMapper;
+class Route implements Guard, ZfcAclAwareInterface
+{
+    /**
+     * @var AclService
+     */
     protected $aclService;
-    
-    public function onRoute(MvcEvent $e) {
+    protected $routeResourceMapMapper;
+
+    public function onRoute(MvcEvent $e)
+    {
         $routeMatch = $e->getRouteMatch();
         $routeName = $routeMatch->getMatchedRouteName();
-        
+
         $map = $this->getRouteResourceMapMapper()->findByRouteName($routeName);
-        if($map === null) {
+        if ($map === null) {
             return;
         }
         $routeResource = $map->getRouteResource($routeName);
-        if($routeResource === null) {
+        if ($routeResource === null) {
             //$routeResource = $this->getDefaultRouteResource();
             //matuszemi: TODO what in this case???
             throw new NoRouteResourceFoundException("No route resource found");
         }
-        
+
         $acl = $this->getAclService();
-        if(!$acl->isAllowed($routeResource)) {
+        if (!$acl->isAllowed($routeResource)) {
             $roleId = $acl->getRole()->getRoleId();
             throw new UnauthorizedException("You ($roleId) are not allowed to access this route '$routeName' ($routeResource)");
         }
     }
-    
+
     //setters/getters
-    public function getRouteResourceMapMapper() {
+    public function getRouteResourceMapMapper()
+    {
         return $this->routeResourceMapMapper;
     }
 
-    public function setRouteResourceMapMapper($routeResourceMapMapper) {
+    public function setRouteResourceMapMapper($routeResourceMapMapper)
+    {
         $this->routeResourceMapMapper = $routeResourceMapMapper;
     }
-        
-    public function getAclService() {
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setZfcAclService(AclService $acl)
+    {
+        $this->aclService = $acl;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getZfcAclService()
+    {
         return $this->aclService;
     }
-
-    public function setAclService($aclService) {
-        $this->aclService = $aclService;
-    }
-
 }
