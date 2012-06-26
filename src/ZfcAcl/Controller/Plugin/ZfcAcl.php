5a280fca14503eabb480d756739675547bed4794
Oskar Bley <oskar@programming-php.net>
2012-06-27 01:10:20 +0200

    cs-fixes

--- src/ZfcAcl/Controller/Plugin/ZfcAcl.php
+++ src/ZfcAcl/Controller/Plugin/ZfcAcl.php
@@ -7,18 +7,18 @@ use Zend\Mvc\Controller\Plugin\AbstractPlugin;
 class ZfcAcl extends AbstractPlugin
 {
     protected $aclService;
-    
+
     public function isAllowed($resource, $privilege = null)
     {
         return $this->getAclService()->isAllowed($resource, $privilege);
     }
-    
-    public function getAclService ()
+
+    public function getAclService()
     {
         return $this->aclService;
     }
 
-    public function setAclService ($aclService)
+    public function setAclService($aclService)
     {
         $this->aclService = $aclService;
     }
